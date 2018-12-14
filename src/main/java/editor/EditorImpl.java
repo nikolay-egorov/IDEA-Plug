3c42eb0ab35639869384c14486fed42188a197c1
nikolay-egorov <deathadder98@gmail.com>
2018-12-14 18:17:51 +0300

    Started to implement CustomFileProvider

---
+++ src/main/java/editor/EditorImpl.java
@@ -0,0 +1,38 @@
+package editor;
+
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.vfs.VirtualFile;
+
+import javax.swing.*;
+
+public class EditorImpl implements EditorInterface{
+
+    private VirtualFile file;
+    private Project project;
+    private  boolean isDisposed;
+
+    @Override
+    public Project getProject() {
+        return project;
+    }
+
+    @Override
+    public JComponent getJComponent() {
+        return null;
+    }
+
+    @Override
+    public JComponent getContentComponent() {
+        return null;
+    }
+
+    @Override
+    public VirtualFile getFile() {
+        return file;
+    }
+
+    @Override
+    public void dispose() {
+        isDisposed=true;
+    }
+}
---
+++ src/main/java/editor/EditorInterface.java
@@ -0,0 +1,19 @@
+package editor;
+
+import com.intellij.openapi.Disposable;
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.vfs.VirtualFile;
+
+import javax.swing.*;
+
+public interface  EditorInterface extends Disposable {
+
+
+    Project getProject();
+
+    JComponent getJComponent();
+
+    JComponent getContentComponent();
+
+    VirtualFile getFile();
+}
