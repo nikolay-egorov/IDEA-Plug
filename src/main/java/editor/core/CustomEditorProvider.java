e9742bf8f41d7d8a5b5790eb675b30118349eaf9
nikolay-egorov <deathadder98@gmail.com>
2018-12-14 22:34:05 +0300

    Class Structure; CustomFileEditorProvider

---
+++ src/main/java/editor/core/CustomEditorProvider.java
@@ -0,0 +1,42 @@
+package editor.core;
+
+import com.intellij.openapi.fileEditor.FileEditorProvider;
+import com.intellij.openapi.fileEditor.FileEditor;
+import com.intellij.openapi.fileEditor.FileEditorPolicy;
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.vfs.VirtualFile;
+import org.jetbrains.annotations.NonNls;
+import org.jetbrains.annotations.NotNull;
+
+public class CustomEditorProvider implements FileEditorProvider {
+
+
+    @NonNls
+    private static final String EXTENSION = "yuml";
+    @NonNls
+    private static final String EDITOR_TYPE_ID = "yumlDiagrams";
+
+
+    @Override
+    public boolean accept(@NotNull Project project, @NotNull VirtualFile virtualFile) {
+        return virtualFile.getExtension().equalsIgnoreCase(EXTENSION);
+    }
+
+    @NotNull
+    @Override
+    public FileEditor createEditor(@NotNull Project project, @NotNull VirtualFile virtualFile) {
+       return new DiagramFileImpl(project,virtualFile);
+    }
+
+    @NotNull
+    @Override
+    public String getEditorTypeId() {
+        return EDITOR_TYPE_ID;
+    }
+
+    @NotNull
+    @Override
+    public FileEditorPolicy getPolicy() {
+        return FileEditorPolicy.PLACE_AFTER_DEFAULT_EDITOR;
+    }
+}
